1aea14849aaef44c5b07d3a040ba12c23c463f71
Jon Stokes <jon@jonstokes.com>
2016-09-29 09:26:33 -0500

    Write out schema in graphql format

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-var express = require('express');
-var compression = require('compression');
-var fs = require('fs');
-var app = express();
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+import schema from './src/schema';
 
-var indexFile = fs.readFileSync('index.html', 'utf8');
-app.use(compression());
-app.get('/', function(req, res) {
-  res.type('html');
-  res.status(200).send(indexFile);
+(function(){
+  var path = require('path');
+  var fs = require('fs');
+  var graphql = require('graphql');
+  var printSchema = require('graphql/utilities').printSchema;
+  var graphQLFile = path.join(__dirname, './schema.graphql');
+
+  fs.writeFileSync(graphQLFile, printSchema(schema));
+}());
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
 });
-app.use('/static', express.static('static'));
-
-var port = process.env.PORT || 3000
-app.listen(port);
-console.log("App started on port: " + port);
